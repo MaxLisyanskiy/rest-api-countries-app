f0761320f6cf586945e5dae901309a93bc0b7918
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2022-05-23 12:46:50 +0300

    feat: added region reducer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,14 +8,14 @@ import { Controls } from '../components/Controls';
 
 import { loadingCountries } from '../store/countries/countries-actions';
 import { selectCountriesInfo, selectVisibleCountries } from '../store/countries/countries-selectors';
-import { selectSearch } from '../store/controls/controls-selectors';
+import { selectControls } from '../store/controls/controls-selectors';
 
 export const HomePage = () => {
   const navigate = useNavigate(),
         dispatch = useDispatch();
 
-  const search = useSelector(selectSearch)
-  const countries = useSelector(state => selectVisibleCountries(state, {search}));
+  const {search, region} = useSelector(selectControls)
+  const countries = useSelector(state => selectVisibleCountries(state, {search, region}));
   const {status, error, qty} = useSelector(selectCountriesInfo);
 
 
--- src/store/controls/controls-actions.js
+++ src/store/controls/controls-actions.js
@@ -1,5 +1,5 @@
 export const SET_SEARCH = '@@controls/SET_SEARCH'
-// export const SET_LOADING = '@@countries/SET_LOADING'
+export const SET_REGION = '@@countries/SET_REGION'
 // export const SET_ERROR = '@@countries/SET_ERROR'
 
 export const setSearch = search => ({
@@ -7,20 +7,8 @@ export const setSearch = search => ({
 	payload: search,
 })
 
-// export const setLoading = () => ({
-// 	type: SET_LOADING,
-// })
+export const setRegion = (region) => ({
+	type: SET_REGION,
+	payload: region
+})
 
-// export const setError = err => ({
-// 	type: SET_ERROR,
-// 	payload: err,
-// })
-
-// export const loadingCountries = () => (dispatch, _, { client, api }) => {
-// 		dispatch(setLoading())
-
-// 		client
-// 			.get(api.ALL_COUNTRIES)
-// 			.then(({ data }) => dispatch(setCountries(data)))
-// 			.catch(err => dispatch(setError(err.message)))
-// 	}
